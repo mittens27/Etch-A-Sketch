9d1392e3cd11564568465afa2c42db8c37f642ce
Andy <amluck88@gmail.com>
2024-02-20 17:58:48 -0500

    added button and code to change size of grid

--- javascript.js
+++ javascript.js
@@ -1,21 +1,50 @@
 const grid = document.querySelector('.grid');
 
+let gridSize = 256;
+let cellRowAmount = 256;
 
+createGrid();
 
-for (var i = 1; i <= 256; i++) {
-    var cell = document.createElement('div');
+document.querySelector('button').addEventListener("click", function() {
+    let gridPrompt = prompt("Please enter desired grid width");
 
-    cell.className = 'cell';
+    if (gridPrompt > 100) {
+        alert("Error. Maximum size is 100 pixels.")
+    } else {
+        gridSize = (gridPrompt ** 2);
+        cellRowAmount = (16 * gridPrompt);
+        clearGrid();
+        createGrid();
+    }
+});
 
-    cell.style.width = "16px";
-    cell.style.height = "16px";
-    cell.style.border = "1px solid black";
-    cell.style.flexBasis = "16px";
-    cell.style.boxSizing = "border-box";
+function createGrid() {
 
-    cell.addEventListener('mouseover', function() {
-        this.style.backgroundColor = "red";
-    });
+    grid.style.maxHeight = `${cellRowAmount}px`;
+    grid.style.maxWidth = `${cellRowAmount}px`;
 
-    grid.appendChild(cell);
+    for (let i = 1; i <= gridSize; i++) {
+        let cell = document.createElement('div');
+
+        cell.className = 'cell';
+
+        cell.style.backgroundColor = "white";
+        cell.style.width = "16px";
+        cell.style.height = "16px";
+        cell.style.border = "1px solid black";
+        cell.style.flexBasis = "16px";
+        cell.style.boxSizing = "border-box";
+
+        cell.addEventListener('mouseover', function() {
+            this.style.backgroundColor = "red";
+        });
+
+        grid.appendChild(cell);
+    }
+}
+
+function clearGrid() {
+    while(grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
 }
