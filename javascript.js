454c3f1163bc9446fe74853033f0a7fa4298b0a8
Andy <amluck88@gmail.com>
2024-02-20 16:42:20 -0500

    add event listener to color cells red on mouseover

--- javascript.js
+++ javascript.js
@@ -1,14 +1,21 @@
 const grid = document.querySelector('.grid');
 
 
+
 for (var i = 1; i <= 256; i++) {
     var cell = document.createElement('div');
+
+    cell.className = 'cell';
 
     cell.style.width = "16px";
     cell.style.height = "16px";
     cell.style.border = "1px solid black";
     cell.style.flexBasis = "16px";
     cell.style.boxSizing = "border-box";
+
+    cell.addEventListener('mouseover', function() {
+        this.style.backgroundColor = "red";
+    });
 
     grid.appendChild(cell);
 }
